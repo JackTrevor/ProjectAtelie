840038557802e957aee83e65ee75a63488d42afc
Jack Trevor <sbrobools@gmail.com>
2023-07-19 14:07:13 -0300

    first commit

---
+++ js/models.js
@@ -0,0 +1,11 @@
+let modelsJson = [
+    {id:1, name:'Fish HandMade Sculpture', img:'img/products/art3.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'Wooden fishes crafted by a local artisan'},
+    {id:2, name:'Wooden Flowers', img:'img/products/art6.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'A kit of 3 wooden flowers craftted by a local artisan'},
+    {id:3, name:'Wooden Caracters', img:'img/products/art7.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'A kit of many local caracters typical from the region'},
+    {id:4, name:'Old Church', img:'img/products/fram3.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'Frames that shows historical buildings of Petrolina'},
+    {id:5, name:'Beautiful Flower Frame', img:'img/products/fram4.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'A set of 3 frames with images of flowers'},
+    {id:6, name:'Bridge Frame', img:'img/products/fram5.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'A set of 3 frames that builds the bridge that connects Petrolina to Juazeiro'},
+    {id:7, name:'Circular Mirror', img:'img/products/mirror4.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'A circular mirror'},
+    {id:8, name:'Samambaia', img:'img/products/plant2.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'A grown Samambaia'},
+    {id:9, name:'Clay Jar', img:'img/products/art8.jpg', price:[100.00, 150.00, 200.00], sizes:['1', '2', '3'], description:'2 Clay Pitchers '},
+];
