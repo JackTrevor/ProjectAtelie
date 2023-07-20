11d3d458858ddcb1a03e2274a67056c87d30c479
Jack Trevor <sbrobools@gmail.com>
2023-07-19 23:17:58 -0300

    Css features changed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let cart = [];
-let modalQt = 0;
+let modalAmt = 0;
 let key = 0;
 const c = (el)=>document.querySelector(el); 
 const cs = (el)=>document.querySelectorAll(el); 
@@ -14,13 +14,13 @@ modelsJson.map((item, index)=>{
     modelsItem.querySelector('.models-item--price').innerHTML = `R$ ${item.price[0].toFixed(2)}`;
     
     modelsItem.querySelector('.models-item--name').innerHTML = item.name;
-    modelsItem.querySelector('.models-item--desc').innerHTML = item.description;
+    modelsItem.querySelector('.models-item--disc').innerHTML = item.description;
     
     modelsItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault(); 
         
         key = e.target.closest('.models-item').getAttribute('data-key'); 
-        modalQt = 1;
+        modalAmt = 1;
        
         c('.modelsBig img').src = modelsJson[key].img;
         c('.modelsInfo h1').innerHTML = modelsJson[key].name;
@@ -35,7 +35,7 @@ modelsJson.map((item, index)=>{
           
             size.querySelector('span').innerHTML = modelsJson[key].sizes[sizeIndex];
         });
-        c('.modelsInfo--amt').innerHTML = modalQt;
+        c('.modelsInfo--amt').innerHTML = modalAmt;
    
         c('.modelsWindowArea').style.opacity = 0; 
        
@@ -63,15 +63,15 @@ cs('.modelsInfo--cancelButton, .modelsInfo--cancelMobileButton').forEach((item)=
 });
 
 c('.modelsInfo--amtless').addEventListener('click', ()=>{
-    if(modalQt > 1) {
-        modalQt--;
-        c('.modelsInfo--amt').innerHTML = modalQt;
+    if(modalAmt > 1) {
+        modalAmt--;
+        c('.modelsInfo--amt').innerHTML = modalAmt;
     }
 });
 
 c('.modelsInfo--amtmore').addEventListener('click', ()=>{
-    modalQt++;
-    c('.modelsInfo--amt').innerHTML = modalQt;
+    modalAmt++;
+    c('.modelsInfo--amt').innerHTML = modalAmt;
 });
 
 cs('.modelsInfo--size').forEach((size, sizeIndex)=>{
@@ -92,13 +92,13 @@ c('.modelsInfo--addButton').addEventListener('click', ()=>{
     let locaId = cart.findIndex((item)=>item.identifier == identifier);
     
     if(locaId > -1){
-        cart[locaId].qt += modalQt;
+        cart[locaId].qt += modalAmt;
     } else {
         cart.push({
             identifier,
             id:modelsJson[key].id,
             size,
-            qt:modalQt
+            qt:modalAmt
         });
     }
     updateCart();
@@ -124,7 +124,7 @@ function updateCart() {
         c('.cart').innerHTML = ''; 
        
         let subtotal = 0;
-        let desconto = 0;
+        let discount = 0;
         let total = 0;
         cart.map((itemCart, index)=>{
             let modelItem = modelsJson.find((itemBD)=>itemBD.id == itemCart.id);
@@ -135,13 +135,13 @@ function updateCart() {
             let modelSizeName;
             switch(itemCart.size) {
                 case 0:
-                    modelSizeName = 'P';
+                    modelSizeName = 'S';
                     break;
                 case 1:
                     modelSizeName = 'M';
                     break;
                 case 2:
-                    modelSizeName = 'G';
+                    modelSizeName = 'B';
             }
             cartItem.querySelector('img').src = modelItem.img;
             
@@ -161,10 +161,10 @@ function updateCart() {
             });
             c('.cart').append(cartItem);
         });
-        desconto = subtotal * 0.1;
-        total = subtotal - desconto;
+        discount = subtotal * 0.1;
+        total = subtotal - discount;
         c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
-        c('.discount span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.discount span:last-child').innerHTML = `R$ ${discount.toFixed(2)}`;
         c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
     } else {
         c('aside').classList.remove('show');
